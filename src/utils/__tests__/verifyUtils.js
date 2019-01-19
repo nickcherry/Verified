46577651f1a35a93d2a24ca129830bf183d7a000
Nick Cherry <nickcherryjiggz@gmail.com>
2019-01-19 17:37:18 -0500

    Implement claim-finding util function

--- src/utils/__tests__/verifyUtils.js
+++ src/utils/__tests__/verifyUtils.js
@@ -1,26 +1,39 @@
-import { getVerificationClaims } from '../verifyUtils';
+import { getClaims } from '../verifyUtils';
 
 describe('verifyUtils', () => {
-  describe('getVerificationClaims', () => {
+  describe('getClaims', () => {
     it('should find a single address', () => {
       const address = '0x52908400098527886E0F7030069857D2E4169EE7';
-      const text = `Some Text verified:${address} Some More Text`;
-      expect(getVerificationClaims(text)).to.deep.eq([address]);
+      const text = `Lorem ipsum verified-start:${address} abc verified-end:${address} dolor sit`;
+      const claims = getClaims(text);
+      expect(claims.length).to.eq(1);
+      expect(claims[0]).to.deep.eq({ address, innerContent: ' abc ' });
     });
 
     it('should find multiple addresses', () => {
       const address1 = '0x52908400098527886E0F7030069857D2E4169EE7';
       const address2 = '0x52908400098527886E0F7030069857D2E4169EE8';
       
-      const text = `Some Text verified:${address1} Some More Text verified:${address2}`;
-      expect(getVerificationClaims(text)).to.deep.eq([address1, address2]);
+      const text = `
+        Lorem ipsum verified-start:${address1} abc verified-end:${address1}
+        dolor sit verified-start:${address2}xyzverified-end:${address2}
+      `;
+
+      const claims = getClaims(text);
+      expect(claims.length).to.eq(2);
+      expect(claims[0]).to.deep.eq({ address: address1, innerContent: ' abc ' });
+      expect(claims[1]).to.deep.eq({ address: address2, innerContent: 'xyz' });
     });
 
-    it('should gracefully handle no matches', () => {
-      expect(getVerificationClaims('verified: 0x52908400098527886E0F7030069857D2E4169EE7')).to.deep.eq([]);
-      expect(getVerificationClaims('verified:0x52908400098527886E0F7030069857D2E4169EEx')).to.deep.eq([]);
-      expect(getVerificationClaims('verified0x52908400098527886E0F7030069857D2E4169EE7')).to.deep.eq([]);
-      expect(getVerificationClaims('verified:')).to.deep.eq([]);
+
+    it('should gracefully handle malformed and non-existent matches', () => {
+      const address = '0x52908400098527886E0F7030069857D2E4169EE7';
+      const shortedAddress = address.slice(0, -1);
+      expect(getClaims(`verified-start:${address} abc`)).to.deep.eq([]);
+      expect(getClaims(`verified-start: ${address} abc verified-end:${address}`)).to.deep.eq([]);
+      expect(getClaims(`verified-start:${shortedAddress} abc verified-end:${shortedAddress}`)).to.deep.eq([]);
+      expect(getClaims(`verified-start${address} abc verified-end:${address}`)).to.deep.eq([]);
+      expect(getClaims(`verified-start:${address}`)).to.deep.eq([]);
     });
   });
 });
--- src/utils/verifyUtils.js
+++ src/utils/verifyUtils.js
@@ -1,5 +1,17 @@
-const verificationClaimsRegex = /verified:0x[a-fA-F0-9]{40}/g;
+const addressRegexSubstr = '0x[a-fA-F0-9]{40}';
+const startTokenLength = 'verified-start:0x'.length + 40;
 
-export const getVerificationClaims = (text) => {
-  return (text.match(verificationClaimsRegex) || []).map((match) => match.replace('verified:', ''));
+export const getClaims = (text) => {
+  const claimsRegex = new RegExp(`verified\\-start:(${addressRegexSubstr})`, 'g');
+  const claims = [];
+  let match;
+  while ((match = claimsRegex.exec(text)) !== null) {
+    const { 1: address, index: startIndex } = match;
+    const endIndex = text.indexOf(`verified-end:${address}`, startIndex);
+    if (endIndex !== -1) {
+      const innerContent = text.substring(startIndex + startTokenLength, endIndex);
+      claims.push({ address, innerContent });
+    }
+  }
+  return claims;
 };
